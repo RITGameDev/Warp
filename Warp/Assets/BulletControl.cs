ea81496ce3533666b0f5b3b9d2677bbb25cfc202
The Stick <avaera8820@gmail.com>
2014-02-20 23:15:52 -0500

    Character Placeholder
    Move, Jump, and Shoot.  Basics.

---
+++ Warp/Assets/BulletControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletControl : MonoBehaviour {
+    [Range(0.5f, 100f)]
+    public float BulletSpeed = 0.5f;
+
+    void FixedUpdate() {
+        transform.Translate(new Vector3(BulletSpeed, 0, 0) * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D data) {
+        Destroy(this.gameObject);
+    }
+}
