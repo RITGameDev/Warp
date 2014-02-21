448792356373f636beb852219b9c3b2ac4983b3f
The Stick <avaera8820@gmail.com>
2014-02-20 23:18:04 -0500

    Character Placeholder
    Move, Jump, Shoot.  Basics.

--- Warp/Assets/CamControl.cs
+++ Warp/Assets/CamControl.cs
@@ -1,1 +1,10 @@
+using UnityEngine;
+using System.Collections;
 
+public class CamControl : MonoBehaviour {
+    public Transform target;
+
+    void FixedUpdate() {
+        transform.position = target.position + new Vector3(0, 0, -10);
+    }
+}
---
+++ Warp/Assets/CharControl.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharControl : MonoBehaviour {
+    [Range(0.1f, 30f)]
+    public float CharSpeed = 1f;
+
+    [Range(1f, 20f)]
+    public float JumpForce = 1f;
+
+    public Transform GroundCheck;
+    public float GroundRadius = 0.2f;
+    private bool grounded = false;
+    public LayerMask WhatIsGround;
+
+    public GameObject Bullet;
+    public Transform BulletSpawn;
+    [Range(0.1f, 10f)]
+    public float ShootCooldownLength = 4f;
+    private float ShootCooldown = 0f;
+
+	// Use this for initialization
+	void Start() {
+
+	}
+
+	void FixedUpdate() {
+        grounded = Physics2D.OverlapCircle(GroundCheck.position, GroundRadius, WhatIsGround);
+
+		transform.Translate(new Vector3(Input.GetAxis("Horizontal") * CharSpeed,0,0));
+	}
+	
+	// Update is called once per frame
+	void Update() {
+        if(grounded && Input.GetButtonDown("Jump")) {
+            rigidbody2D.AddForce(new Vector2(0, 100 * JumpForce));
+        }
+
+        if(ShootCooldown == 0f && Input.GetButtonDown("Fire")) {
+            Instantiate(Bullet, BulletSpawn.position, Quaternion.identity);
+            ShootCooldown = ShootCooldownLength;
+        }
+
+        if(ShootCooldown > 0f) {
+            ShootCooldown = Mathf.Max(0f, ShootCooldown - Time.deltaTime);
+        }
+	}
+}
