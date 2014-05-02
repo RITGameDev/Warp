3fac658fdf47a984fbd047047ea2111565dedf9a
Yawar Raza <ysr7349@rit.edu>
2014-05-01 20:29:25 -0400

    Creates portals on bullet-wall collision
    PORTALZ!!!

--- Warp/Assets/BulletControl.cs
+++ Warp/Assets/BulletControl.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BulletControl : MonoBehaviour {
-    [Range(0.5f, 100f)]
-    public float BulletSpeed = 0.5f;
+public class BulletControl : MonoBehaviour
+{
+		[Range(0.5f, 100f)]
+		public float
+				BulletSpeed = 0.5f;
 
-    void FixedUpdate() {
-        transform.Translate(new Vector3(BulletSpeed, 0, 0) * Time.deltaTime);
-    }
+		void FixedUpdate ()
+		{
+				transform.Translate (new Vector3 (BulletSpeed, 0, 0) * Time.deltaTime);
+		}
 
-    void OnCollisionEnter2D(Collision2D data) {
-        Destroy(this.gameObject);
-    }
+		void OnCollisionEnter2D (Collision2D data)
+		{
+				Destroy (this.gameObject);
+				if (data.collider.gameObject.layer == LayerMask.NameToLayer ("Ground")) {
+						Transform portal = GameObject.Find ("GameManager").GetComponent<GameManager> ().Portal;
+						Instantiate (portal, data.contacts [0].point + data.contacts [0].normal.normalized * portal.GetComponent<BoxCollider2D> ().size.x / 2, Quaternion.identity);
+				}
+		}
 }
--- Warp/Assets/GameManager.cs
+++ Warp/Assets/GameManager.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameManager : MonoBehaviour {
-    public bool dispMenu;
+public class GameManager : MonoBehaviour
+{
+		public bool dispMenu;
 
-    public Transform SpawnPoint;
+		public Transform SpawnPoint;
 	
-	public Transform TeleportNodePairPrefab;
+		public Transform TeleportNodePairPrefab;
+
+		public Transform Portal;
 }
